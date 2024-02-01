40b36c06fc63c739898cb19445e1bb409d15ba82
AlekseySamokhvalov <6560@mail.ru>
2024-02-02 02:10:17 +0300

    Научились реагировать на команду /start и выводить приветственное сообщение.

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            Long chatId = update.message().chat().id();
+            if(update.message().text().equals("/start")) {
+                SendMessage message = new SendMessage(chatId, String.format("Привет, %s!", update.message().from().firstName()));
+                telegramBot.execute(message);
+            }
             // Process your updates here
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
